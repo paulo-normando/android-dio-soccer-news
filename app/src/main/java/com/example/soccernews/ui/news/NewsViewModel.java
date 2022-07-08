e424dcf9f50bd20537eab12df2885d5ede35576b
Paulo Normando <paulonormando@gmail.com>
2022-07-08 20:45:34 -0300

    Ajuste do bottom_nav_menu - Fragmentos e ViewModels

--- app/src/main/java/com/example/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/com/example/soccernews/ui/news/NewsViewModel.java
@@ -1,14 +1,14 @@
-package com.example.soccernews.ui.home;
+package com.example.soccernews.ui.news;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class NewsViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public NewsViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
     }

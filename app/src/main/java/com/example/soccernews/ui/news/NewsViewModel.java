e44a84a25ac5f881b3b6b1f4bfda5bcdf313b048
Paulo Normando <paulonormando@gmail.com>
2022-07-09 10:14:31 -0300

    Criado package domain - Classe News.java
    Adicionado mock de noticias em NewsViewModel.java

--- app/src/main/java/com/example/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/com/example/soccernews/ui/news/NewsViewModel.java
@@ -3,17 +3,28 @@ package com.example.soccernews.ui.news;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import com.example.soccernews.domain.News;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NewsViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<List<News>> news;
 
     public NewsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        this.news = new MutableLiveData<>();
+
+        //TODO remover mock de noticias
+        List<News> news = new ArrayList<>();
+        news.add(new News("Paysandu vence Remo no domingo","Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."));
+        news.add(new News("Paysandu joga no sábado","Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."));
+        news.add(new News("Copa do mundo feminina inicia","Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."));
+
+        this.news.setValue(news);
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<List<News>> getNews() {
+        return news;
     }
 }
